50f9c9ba024d371b7478fd976e2c0ed1c8bc47e1
Filip Lindqvist <buggfille@gmail.com>
2017-11-15 23:14:53 +0100

    Changed parameters, allowed defaults, changed to const

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-var request = require("request");
-var WebSocket = require("ws");
-var wol = require("wake_on_lan");
+const request = require("request");
+const WebSocket = require("ws");
+const wol = require("wake_on_lan");
 
-function SamsungTV(macAddress, ipAddress, port, appName) {
-  this.macAddress = macAddress;
+const SamsungTV = module.exports = function SamsungTV(ipAddress, macAddress, port, appName) {
   this.ipAddress = ipAddress;
-  this.port = port;
-  this.appName = appName;
+  this.port = port || "8001";
+  this.macAddress = macAddress || "00:00:00:00:00:00";
+  this.appName = appName || __dirname;
   this.encodedAppName = new Buffer(this.appName).toString("base64");
+
+  if (this.macAddress === "00:00:00:00:00:00") {
+    console.log("Warning: no MAC address specifed, won't be able to wake the device..");
+  }
 }
 
-module.exports = SamsungTV;
+const getUrl = function (self, path) {
+  return "http://" + self.ipAddress + ":" +  self.port + "/api/v2/" + (path || "");
+}
 
 SamsungTV.prototype.sendKey = function(key, callback) {
-  var url = "http://" + this.ipAddress + ":8001/api/v2/channels/samsung.remote.control?name=" + this.encodedAppName
-  var ws = new WebSocket(url, callback)
+  const url = getUrl(this, "channels/samsung.remote.control?name=" + this.encodedAppName)
+  const ws = new WebSocket(url, callback)
   ws.on("error", callback)
   ws.on("message", function(data, flags) {
-    var cmd = {
+    const cmd = {
       "method": "ms.remote.control",
       "params": {
         "Cmd": "Click",
@@ -34,7 +40,7 @@ SamsungTV.prototype.sendKey = function(key, callback) {
 }
 
 SamsungTV.prototype.waitForTVOn = function(attemptsLeft, callback) {
-  var accessory = this;
+  const accessory = this;
   wol.wake(this.macAddress, function(err) {
     accessory.isOn(function(err, isOn) {
       if (isOn) {
@@ -59,7 +65,7 @@ SamsungTV.prototype.turnOff = function(callback) {
 }
 
 SamsungTV.prototype.isOn = function(callback) {
-  var url = "http://" + this.ipAddress + ":" + this.port + "/api/v2/"
+  const url = getUrl(this);
   return request.get(url, { timeout: 5000 }, function(err, httpResponse, body) {
     if (err || httpResponse.statusCode != 200) {
       callback(null, false)
@@ -79,14 +85,14 @@ SamsungTV.prototype.volumeDown = function(callback) {
 
 SamsungTV.prototype.changeVolume = function(volumeChange, callback) {
   if (volumeChange != 0) {
-    var samsungtv = this
-    var key = volumeChange > 0 ? "KEY_VOLUP" : "KEY_VOLDOWN"
+    const samsungtv = this
+    const key = volumeChange > 0 ? "KEY_VOLUP" : "KEY_VOLDOWN"
     this.sendKey(key, function(error) {
       if (error) {
         return callback(error)
       } else {
         setTimeout(function() {
-          var next = volumeChange > 0 ? volumeChange-1 : volumeChange+1
+          const next = volumeChange > 0 ? volumeChange-1 : volumeChange+1
           samsungtv.changeVolume(next, callback)
         }, 1000)
       }
